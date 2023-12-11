08b8393032d71a1c5d58c2f347d30c0b63fc1e70
Sebastian Schavon <sebastian.schavon@hotmail.se>
2023-12-11 01:22:41 +0100

    Day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,16 @@
+﻿var path = "strings.txt";
+var readText = File.ReadAllLines(path).ToList();
+
+var sum = Aggregate(readText).Sum();
+
+Console.WriteLine(sum);
+Console.ReadLine();
+
+static List<int> Aggregate(List<string> strings) => strings.Aggregate(new List<int>(), (sum, val) => 
+{
+    var numbers = GetNumbers(val);
+    sum.Add(int.Parse($"{numbers[0]}{numbers[^1]}"));
+    return sum;
+});
+
+static char[] GetNumbers(string val) => val.Where(Char.IsDigit).ToArray();
